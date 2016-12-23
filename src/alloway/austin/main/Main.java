15708520f31ba089bb33e0adb0b6b38d9347e6fd
DockingMule <aca0824@gmail.com>
2016-12-22 21:04:09 -0500

    added "Methods" class

--- src/alloway/austin/main/Main.java
+++ src/alloway/austin/main/Main.java
@@ -13,23 +13,9 @@ import java.util.List;
 
 public class Main {
 
-    public static void inBinary(int initial) {
-        int [] list = new int [] {128,64,32,16,8,4,2,1};
-        int current = initial;
-        int [] binary = new int [] {0,0,0,0,0,0,0,0};
-        for (int i = 0; i < list.length; i++){
-            if (current<list[i])
-                binary[i] = 0;
-            else{
-                current -= list[i];
-                binary[i]=1;}}
-        for (int i = 0; i < list.length; i++){
-            System.out.print(binary[i] + " ");}
-    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        inBinary(a);
+
     }
 }
